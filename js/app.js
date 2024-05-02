7f559b3414173b811329433442604eab5cd01308
Jacopo <j.dedominicis@studenti.unisa.it>
2024-05-02 16:00:36 +0200

    Add verify function

--- js/app.js
+++ js/app.js
@@ -1,7 +1,9 @@
 //import { enableMasca, isError } from '@blockchain-lab-um/masca-connector';
 
-const connetti = document.getElementById("connettiMasca");
+import {enableMasca} from "@blockchain-lab-um/masca-connector";
 
+const connetti = document.getElementById("connettiMasca");
+var mascaApi;
 connetti.addEventListener("click", async function() {
   window.ethereum.request({
     method: 'eth_requestAccounts',
@@ -10,7 +12,7 @@ connetti.addEventListener("click", async function() {
     alert("ADDRESS: " + address);
 
     const enableResult = await enableMasca(address);
-
+    mascaApi = await enableResult.data.getMascaApi();
   }).catch(error => {
     console.error("ERRORE CON METAMASK " + error);
   })
@@ -21,16 +23,41 @@ connetti.addEventListener("click", async function() {
 const verifica = document.getElementById("verificaVP");
 
 verifica.addEventListener("click", async function () {
-  const vp = document.getElementById("vpText").value;
-  if (vp.length === 0) alert("Inserisci una VP");
+  const vpText = document.getElementById("vpText").value;
+  if (vpText.length === 0) alert("Inserisci una VP");
   else {
-    const vpRes = await api.verifyData({presentation: vp, verbose: true});
+    alert("Hai inserito: " + vpText);
 
-    if (vpRes) {
+    const vp = mascaCreatePresentation(vpText);
+
+    const vpRes = await mascaApi.verifyData({presentation: vp, verbose: true});
+
+    /*if (vpRes) {
       alert("La VP è corretta");
     } else {
       alert("La VP è sbagliata");
-    }
+    }*/
 
   }
 });
+
+async function mascaCreatePresentation(vpText) {
+  const vc = mascaCreateCredential(vpText)
+  alert("VC creata con successo");
+  return await mascaApi.createPresentation({
+    vcs: vc,
+    proofFormat: 'jwt',
+  });
+}
+
+function mascaCreateCredential(payload) {
+  alert("Creazione credenziale verificabile...");
+  return mascaApi.createCredential({
+    minimalUnsignedCredential: payload,
+    proofFormat: 'jwt',
+    options: {
+      save: 'true',
+      store: ['snap'],
+    },
+  });
+}
