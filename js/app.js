e9337e727a9ccdfa94e932391734253dccda3c73
Jacopo <j.dedominicis@studenti.unisa.it>
2024-05-02 14:34:01 +0200

    Initial commit

---
+++ js/app.js
@@ -0,0 +1,18 @@
+import { enableMasca, isError } from '@blockchain-lab-um/masca-connector';
+
+const connetti = document.getElementById("connettiMasca");
+
+connetti.addEventListener("click", async function() {
+  window.ethereum.request({
+    method: 'eth_requestAccounts',
+  }).then(async accounts => {
+    const address = accounts[0];
+    alert("ADDRESS: " + address);
+
+    const enableResult = await enableMasca(address);
+
+  }).catch(error => {
+    console.error("ERRORE CON METAMASK " + error);
+  })
+
+});
