b635024d879be9ba014fd6e199cdfb2882934469
Jacopodd <80394163+Jacopodd@users.noreply.github.com>
2024-05-03 16:14:02 +0200

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,5 +1,3 @@
-//import { enableMasca, isError } from '@blockchain-lab-um/masca-connector';
-
 import {enableMasca} from "@blockchain-lab-um/masca-connector";
 
 var mascaApi;
@@ -19,7 +17,6 @@ connetti.addEventListener("click", async function() {
     address = accounts[0];
 
     const enableResult = await enableMasca(address);
-    nascondiCaricamento();
 
     console.log("EnableResult " + enableResult.success);
 
@@ -30,6 +27,8 @@ connetti.addEventListener("click", async function() {
     }
 
     mascaApi = await enableResult.data.getMascaApi();
+    nascondiCaricamento();
+    mostraForm();
   }).catch(error => {
     console.error("ERRORE CON METAMASK " + error);
   })
@@ -52,11 +51,33 @@ verifica.addEventListener("click", async function() {
     console.log("RESULT: " + vpRes.success);
     if (vpRes.success) {
       mostraSuccesso();
+      inserisciDati(jsonPresentation)
     } else mostraErrore();
 
   } else console.log("Non hai inserito nulla");
-
 })
+
+function inserisciDati(vp) {
+  var vcId = vp.verifiableCredential[0].vc.id;
+  var issuer = vp.verifiableCredential[0].vc.issuer;
+  var issuanceDate = vp.verifiableCredential[0].vc.issuanceDate;
+  var expirationDate = vp.verifiableCredential[0].vc.expirationDate;
+  var type = vp.verifiableCredential[0].type[2];
+  var holder = vp.holder
+  var proof = vp.proof.type;
+  
+  document.getElementById('vc-id').innerHTML = vcId; 
+  document.getElementById('issuer').innerHTML = issuer;
+  document.getElementById('issuance-date').innerHTML = issuanceDate;
+  document.getElementById('expiration-date').innerHTML = expirationDate;
+  document.getElementById('type').innerHTML = type;
+  document.getElementById('holder').innerHTML = holder;
+  document.getElementById('proof').innerHTML = proof;
+}
+
+function mostraForm() {
+  document.getElementById('vp').style.display = 'flex';
+}
 
 function mostraCaricamento() {
   document.getElementById('caricamento').style.display = 'block'; 
@@ -66,12 +87,20 @@ function nascondiCaricamento() {
   document.getElementById('caricamento').innerText = "Connesso con Metamask!";
 }
 
+function mostraBanner() {
+  document.getElementById('risultati').style.visibility = 'visible';
+  document.getElementById('risultati').style.display = 'block'
+}
+
 function mostraSuccesso() {
+  mostraBanner();
   successo.style.display = 'block';
   error.style.display = 'none';
+  mostraResult();
 }
 
 function mostraErrore() {
+  mostraBanner();
   error.style.display = 'block';
   success.style.display = 'none';
 }
@@ -79,4 +108,23 @@ function mostraErrore() {
 function nascondiRisultati() {
   error.style.display = 'none';
   successo.style.display = 'none';
+  document.getElementById('risultati').style.display = 'none';
+  document.getElementById('risultati').style.visibility = 'hidden';
+}
+
+function mostraResult() {
+  document.getElementById('result').style.display = 'flex';
+  document.getElementById('vp').style.display = 'none';
+  document.getElementById('connessione').style.display = 'none';
+}
+
+
+const rip = document.getElementById('riprova');
+rip.addEventListener('click', function() {
+  riprova();
+}); 
+function riprova() {
+  document.getElementById('result').style.display = 'none';
+  document.getElementById('connessione').style.display = 'flex';
+  nascondiRisultati();
 }
